eebaa87f0b0ba1e8bf81289c5389494a55463301
Abdelwahab <aa3662067@gmail.com>
2025-06-23 11:09:50 +0300

    v1-

--- app/(main)/contact/page.tsx
+++ app/(main)/contact/page.tsx
@@ -1,35 +1,38 @@
-import data from "@/data/home.json"
-import dataP from "@/data/projects.json"
-import { timeStamp } from "console";
+import data from "@/data/home.json";
+import dataP from "@/data/projects.json";
 
-const page =  () => {
+interface ContactLink {
+  item: keyof typeof dataP.icon;
+  Linkedin?: string;
+  Github?: string;
+  // Add other possible properties here if needed
+}
 
-const { icon  } = dataP;
-  const { contactLinks} = data;
-
+const Page = () => {
+  const { icon } = dataP;
+  const { contactLinks } = data as { contactLinks: ContactLink[] };
 
   return (
-    <div className="w-full h-screen flex justify-center items-center flex-col gap-[5%]" >
-    
-  
-        <a href='mailto:bad3662067@gmail.com' className="p-[3px] relative cursor-pointer w-fit flex ">
-          <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
-          <div className="px-8 py-2 bg-gray-200 text-black dark:bg-black dark:text-white rounded-[6px]  relative group transition duration-200  hover:bg-transparent font-bold">
-           Let&apos;s get in Touch
-          </div>
+    <div className="w-full h-screen flex justify-center items-center flex-col gap-[5%]">
+      <a href='mailto:bad3662067@gmail.com' className="p-[3px] relative cursor-pointer w-fit flex">
+        <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
+        <div className="px-8 py-2 bg-gray-200 text-black dark:bg-black dark:text-white rounded-[6px] relative group transition duration-200 hover:bg-transparent font-bold">
+          Let&apos;s get in Touch
+        </div>
       </a>
       <div className="flex gap-[5%] p-3 bg-gray-300 rounded-2xl">
-        {contactLinks.map((ele)=>(
-          <a href={ele[ele.item]} target="_blanck" key={ele.item+timeStamp()}>
-
-            <img src={icon[ele.item]} alt={ele.item} className="w-20 h-20"/>
+        {contactLinks.map((ele) => (
+          <a 
+            href={ele[ele.item] as string} 
+            target="_blank" 
+            key={ele.item + Date.now()}
+          >
+            <img src={icon[ele.item]} alt={ele.item} className="w-20 h-20" />
           </a>
         ))}
       </div>
-
-    
     </div>
   );
 };
 
-export default page;
+export default Page;
